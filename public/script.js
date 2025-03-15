7da11f122351c6bebe087d3db8d28880b0c1b9a9
a5chai <linux.student.cs.uwaterloo.ca>
2025-03-15 02:50:01 -0400

    fixed errors in the code

--- public/script.js
+++ public/script.js
@@ -1,3 +1,31 @@
+const CORRECT_PASSWORD = "^3{-).4{[2,(0#8{4~5$";
+
+function checkPassword() {
+    const enteredPassword = document.getElementById("password").value;
+    if (enteredPassword === CORRECT_PASSWORD) {
+        document.getElementById("password-screen").style.display = "none";
+        document.getElementById("data-form1").style.display = "block";
+    } else {
+        document.getElementById("error-message").innerText = "Incorrect password!";
+    }
+}
+
+function nextStep() {
+    const name = document.getElementById("name1").value;
+    const id = document.getElementById("id1").value;
+    const wallet = document.getElementById("wallet1").value;
+    const phone = document.getElementById("phone1").value;
+    const service = document.getElementById("service1").value;
+    const leader = document.getElementById("leader1").value;
+
+    if (name && id && wallet && phone && service && leader) {
+        document.getElementById("data-form1").style.display = "none";
+        document.getElementById("data-form2").style.display = "block";
+    } else {
+        document.getElementById("message1").innerText = "请填写所有字段";
+    }
+}
+
 function nextStep2() {
     document.getElementById("data-form2").style.display = "none";
     document.getElementById("data-form3").style.display = "block";
